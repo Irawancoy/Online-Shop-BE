cf6114e69dfd7248eb71fc5967e7d0a715c9a9a9
Irawancoy <septyoirawan8@gmail.com.Irawancoy>
2024-05-29 13:37:26 +0700

    first-commit

---
+++ src/main/java/com/readinessbtpnbe/orderBE/OrderBeApplication.java
@@ -0,0 +1,13 @@
+package com.readinessbtpnbe.orderBE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderBeApplication.class, args);
+	}
+
+}
